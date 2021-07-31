91c2fbe7ff0afa61c52d6a1a22a81f4dd17e530f
Mystique <13oct08@quantumfoam.uni.cc>
2021-07-31 18:53:10 +0200

    LGTM

--- stacks/miztiik_global_args.py
+++ stacks/miztiik_global_args.py
@@ -6,5 +6,5 @@ class GlobalArgs():
     OWNER = "MystiqueAutomation"
     REPO_NAME = "improve-db-perf-with-rds-proxy"
     SOURCE_INFO = f"https://github.com/miztiik/{REPO_NAME}"
-    VERSION = "2020_04_13"
+    VERSION = "2021_04_13"
     MIZTIIK_SUPPORT_EMAIL = ["mystique@example.com", ]
